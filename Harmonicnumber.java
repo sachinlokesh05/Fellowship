7d757b1a6a430a57dd231c98b4f67b7ace7e681f
sachinlokesh05 <56384957+sachinlokesh05@users.noreply.github.com>
2019-10-10 18:12:28 +0530

    Update Harmonicnumber.java

--- Harmonicnumber.java
+++ Harmonicnumber.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+public class Harmonic_number {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the nth number");
+		int n=sc.nextInt();
+		double sum=0;
+		for(int i=1;i<=n;i++)
+		{
+			
+			sum=sum+(double)1/i;
+			System.out.println(i+":"+sum);
+		}
+			System.out.println(sum);
+			System.out.println((long)(1/3));
+		}
+
+	}
+
+
 
